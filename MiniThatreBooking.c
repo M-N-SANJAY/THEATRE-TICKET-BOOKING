3ff454eb9c05db67e09c441482e76abf02105bd6
MNS <sanjayajnas447@gmail.com>
2025-08-21 01:03:26 +0530

    Update MiniThatreBooking.c

--- MiniThatreBooking.c
+++ MiniThatreBooking.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 
+//initialising the theatre multi dimensional array with 5 rows and 10 columns
 int theatre[5][10];
 
+//function to books seat using row and column
 void seatbooking(int row,int col){
-    if(theatre[row-1][col-1]==1){
-        printf("\nSeat at row %d col %d is already booked, unable to book",row,col);
+    //converting 1-based indexing row,col to 0-based indexing
+    if(theatre[row-1][col-1]==1){       
+        printf("\nSeat at row %d col %d is already booked, unable to book",row,col);  //Pre-Booked
         return;
     }
-    theatre[row-1][col-1]=1;
+    theatre[row-1][col-1]=1;  //Booking                       
     printf("\nSuccessfully booked your seat at row %d col %d",row,col);
 }
 
+//function to cancel pre-booked seat
 void seatcancel(int row,int col){
     if(theatre[row-1][col-1]==0){
-        printf("\nSeat at row %d col %d is not pre-booked, unable to cancel",row,col);
+        // to check whether the seat is already pre-booked first
+        printf("\nSeat at row %d col %d is not pre-booked, unable to cancel",row,col);  
         return;
     }
-    theatre[row-1][col-1]=0;
+    theatre[row-1][col-1]=0;  //Unbooking
     printf("\nSuccesfully un-booked your seat at row %d col %d",row,col);
 }
 
+//checking available seats
 void seatavailability(){
     int count=0;
     printf("\nNote: X - booked , O - Not Booked");
@@ -36,16 +42,22 @@ void seatavailability(){
             }
         }
     }
+    //outputting the count of unbooked seats too
     printf("\nThere are total of %d seats currently available",count);
 }
 
 void main() {
+    
     int i,j,row,col,choice;
+
+    //setting all seats as 0 denoting unbooked, note :- 0 = unbooked, 1 = booked
     for(i=0;i<5;i++){
         for(j=0;j<10;j++){
             theatre[i][j]=0;
         }
     }
+
+    //Menu
     printf("THEATRE STRUCTURE:-\nROWS = 5\nCOLUMNS = 10");
     printf("\n\nChoices Available:-\n1.Book Seat\n2.Cancel Seat Booking\n3.Check Seat Availability\n4.Exit");
     while(1){
@@ -65,7 +77,7 @@ void main() {
             seatavailability();
         }
         else{
-            break;
+            break;   //exit
         }
     }
 }
